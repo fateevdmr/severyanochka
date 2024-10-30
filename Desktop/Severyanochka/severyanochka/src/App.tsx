a8cc965447b7cb2c179d2c5618a6163db433ce30
fateevdmr <110971317+fateevdmr@users.noreply.github.com>
2024-10-30 11:14:49 +0300

    added typescript

--- Desktop/Severyanochka/severyanochka/src/App.tsx
+++ Desktop/Severyanochka/severyanochka/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './Styles/index.css';
-import './index.tsx'
+import './index.tsx';gir
 import Header from './pages/header';
-import Produkts from './pages/produkts'
+import Produkts from './pages/produkts';
 import SpecialOffersMap from './pages/specialOffersMap';
 import Footer from './pages/footer';
 
